419db29edbaed61f17dffc4bc81f078da6c76e4f
Eric YANG <eyang@c02h86nwq05n.ad.mdsol.com>
2022-10-31 09:20:44 -0400

    added in libs

---
+++ tests/lib/SampleLibtest.py
@@ -0,0 +1,10 @@
+import unittest
+from src.lib.sample_lib import add_two_numbers
+
+class TestSampleLib(unittest.TestCase):
+    def test_add_two_numbers(self):
+        self.assertTrue(add_two_numbers(10, 20) == (10 + 20), "Does not do what it says on the tin")
+
+
+if __name__ == '__main__':
+    unittest.main()
