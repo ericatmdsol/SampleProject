a12fa10bfb7f2f52f075cf220580348d8879211a
Eric YANG <eyang@c02h86nwq05n.ad.mdsol.com>
2022-10-31 09:21:24 -0400

    added lib

---
+++ src/lib/sample_lib.py
@@ -0,0 +1,11 @@
+def add_two_numbers(a: int, b: int)->int:
+    """Does what it says on the tin, just to illustrate unit testing
+
+    Args:
+        a (int): one number
+        b (int): another number
+
+    Returns:
+        int: the sum
+    """    
+    return a + b
